8623d6b50cfe1ee7a6364c7c2d417ee85a7cec69
Damian <damian.popielarski@connectis.pl>
2018-04-24 18:20:02 +0200

    Added application main class

--- src/main/kotlin/com/demo/bookingapp/BookingApplication.kt
+++ src/main/kotlin/com/demo/bookingapp/BookingApplication.kt
@@ -1,2 +1,13 @@
 package com.demo.bookingapp
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableAutoConfiguration
+class BookingApplication
+
+fun main(args: Array<String>) {
+    runApplication<BookingApplication>(*args)
+}
